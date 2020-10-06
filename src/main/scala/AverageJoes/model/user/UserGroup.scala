8c0b57ae0b58e37910141c783ab515f1937067d5
Sokol Guri <sokol.guri@studio.unibo.it>
2020-10-07 00:48:17 +0200

    User features tested. OK

--- src/main/scala/AverageJoes/model/user/UserGroup.scala
+++ src/main/scala/AverageJoes/model/user/UserGroup.scala
@@ -14,26 +14,54 @@ object UserGroup {
   def apply(groupID: String): Behavior[Command] = Behaviors.setup(ctx => new UserGroup(ctx, groupID))
 
   trait Command
-  private final case class DeviceTerminated(device: ActorRef[User.Command], groupId: String, userId: String) extends Command
+  private final case class UserTerminated(device: ActorRef[User.Command], groupId: String, userId: String) extends Command
 }
 
 class UserGroup(ctx: ActorContext[UserGroup.Command], groupId: String)
   extends AbstractBehavior[UserGroup.Command](ctx) {
 
-  import UserGroup._
+  import UserGroup.{Command, UserTerminated}
   import UserManager._
 
   private var userIdToActor = Map.empty[String, ActorRef[User.Command]]
 
-  //context.log.info("UserGroup {} started", groupId)
+  println("UserGroup {"+groupId+"} started")
 
-  override def onMessage(msg: UserGroup.Command): Behavior[UserGroup.Command] =
+  override def onMessage(msg: Command): Behavior[Command] =
     msg match {
+      case createUserMsg @ RequestUserCreation(`groupId`, userId, replyTo) =>  // `groupId` is used to check if group param is the same
+        userIdToActor.get(userId) match {
+          case Some(userActor) => replyTo ! UserRegistered(userActor)
+          case None =>
+              println("Creating user actor for {"+createUserMsg.userId+"}")
+              val userActor = context.spawn(User(groupId, userId), s"user-$userId")
+              context.watchWith(userActor, UserTerminated(userActor, groupId, userId))
+              userIdToActor += userId -> userActor
+              replyTo ! UserRegistered(userActor)
+        }
+        this
+
+      case RequestUserCreation(gId,_,_) =>
+        println("Ignoring UserCreation request for {"+gId+"}. This actor is responsible for {"+groupId+"}.")
+        this
+
       case RequestUserList(requestId, gId, replyTo) =>
         if(gId == groupId) {
           replyTo ! ReplyUserList(requestId, userIdToActor.keySet)
           this
-        }else Behaviors.unhandled
+        }
+        else Behaviors.unhandled
+
+      case UserTerminated(_,_, userId) =>
+          println("User actor for {"+userId+"} has been terminated")
+          userIdToActor -= userId
+          this
     }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      println("UserGroup {} stopped", groupId)
+      this
+  }
 
 }
