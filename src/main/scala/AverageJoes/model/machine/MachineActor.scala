5a3671a58a87ac7b6f9306c68a9c4d21abdc4921
Elena_96 <elena.morelli3@studio.unibo.it>
2020-10-14 11:29:36 +0200

    deleted common command

--- src/main/scala/AverageJoes/model/machine/MachineActor.scala
+++ src/main/scala/AverageJoes/model/machine/MachineActor.scala
@@ -1,8 +1,8 @@
 package AverageJoes.model.machine
 
 import java.util.Optional
-
-import AverageJoes.common.Command
+import AverageJoes.model.machine.MachineActor.{BookingStatus, Command, MachineBooking, PMActorStarted, UserLogIn, UserLogInStatus, UserLogOut, UserLoggedInMachine, UserMachineWorkoutCompleted, UserMachineWorkoutPlan, UserRef}
+import AverageJoes.model.workout.Exercise
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 /**
@@ -12,11 +12,23 @@ import akka.actor.typed.{ActorRef, Behavior}
 object MachineActor{
   def apply(controller: ActorRef[Command], machineType: Class[_ <: PhysicalMachine]): Behavior[Command] =
     Behaviors.setup(context => new MachineActor(context, controller, machineType))
+
+  sealed trait Command
+
+  final case class PMActorStarted(replyTo: ActorRef[Command]) extends Command
+  final case class UserLogIn(userID: String) extends Command
+  final case class UserLogInStatus(status: Boolean) extends Command
+  final case class MachineBooking(userID: String, replyTo: ActorRef[BookingStatus]) extends Command
+  final case class BookingStatus(status: Boolean) extends Command
+  final case class UserRef(replyTo: ActorRef[UserLoggedInMachine]) extends Command
+  final case class UserLoggedInMachine() extends Command
+  final case class UserMachineWorkoutPlan (userID: String, exercise: Class[_ <: Exercise]) extends Command
+  final case class UserMachineWorkoutCompleted (user: ActorRef[Command], exercise: Class[_ <: Exercise]) extends Command
+  final case class UserLogOut() extends Command
 }
 
 class MachineActor(context: ActorContext[Command], controller: ActorRef[Command], //da rendere actor ref
                    machineType:Class[_ <: PhysicalMachine]) extends AbstractBehavior[Command](context) {
-  import Command._
 
   var booked: (Boolean, String) = (false, "")
   var physicalMachine: Optional[ActorRef[Command]] = Optional.empty()
