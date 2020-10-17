f5c7439c4e56cef5e8d134b511a9145e9fda01d9
Sokol Guri <sokol.guri@studio.unibo.it>
2020-10-17 20:06:28 +0200

    Entity and storage added

--- src/main/scala/AverageJoes/common/Entity.scala
+++ src/main/scala/AverageJoes/common/Entity.scala
@@ -1,14 +1,16 @@
-package AverageJoes.common.storage
+package AverageJoes.common
+
+import AverageJoes.utils.IdGenerator._
 
 trait Entity {
   def getId: String
 }
 
-abstract class BaseEntity(id: String) extends Entity{
+abstract class BaseEntity(id: String) extends Entity {
 
   /** Secondary constructor **/
   def this(){
-    this(java.util.UUID.randomUUID.toString)
+    this(generateUiid)
   }
 
   override def getId: String = id
