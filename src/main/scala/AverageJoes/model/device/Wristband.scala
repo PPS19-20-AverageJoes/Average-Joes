d1959c3dddf5189cb930ebb7f736c202e550d3ad
Andrea <andrea.campidelli@studio.unibo.it>
2020-10-23 18:01:37 +0200

    PhysicalMachine and Wristband bug solved

--- src/main/scala/AverageJoes/model/device/Wristband.scala
+++ src/main/scala/AverageJoes/model/device/Wristband.scala
@@ -8,7 +8,7 @@ import akka.actor.typed.{ActorRef, Behavior}
  * AC
  * @param deviceID: ID of the device. In real devices, is stored on config files
  */
-class Wristband(context: ActorContext[Device.Msg], deviceID: String) extends AbstractBehavior[Device.Msg](context) with Device {
+class Wristband(context: ActorContext[Device.Msg], override val deviceID: String) extends AbstractBehavior[Device.Msg](context) with Device {
 
   def display (s: String): Unit = {
     println(s)
