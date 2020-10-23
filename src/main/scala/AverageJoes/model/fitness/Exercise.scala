b975f6ea8d795d0b43d4f3bacc2b7feaba082211
Sokol Guri <sokol.guri@studio.unibo.it>
2020-10-23 17:35:22 +0200

    Implicit parameter extractor

--- src/main/scala/AverageJoes/model/fitness/Exercise.scala
+++ src/main/scala/AverageJoes/model/fitness/Exercise.scala
@@ -1,5 +1,6 @@
 package AverageJoes.model.fitness
 
+<<<<<<< 57dee68b993d9a31acc64c9c37d29566fd4cd992
 import AverageJoes.model.fitness.ExerciseExecutionConfig.ExerciseConfiguration.Parameters
 import AverageJoes.model.fitness.ExerciseExecutionConfig.ParameterExtractor
 import AverageJoes.model.fitness.MachineExecution.MachineEquipment
@@ -16,4 +17,3 @@ object Exercise{
     def apply(equipment: MachineEquipment): Exercise  =  ExerciseImpl(equipment)
     private case class ExerciseImpl(equipment: MachineEquipment) extends Exercise
 }
-
