73e3732836c5a20ad8fb5eec8b27aad614b3372f
Sokol Guri <sokol.guri@studio.unibo.it>
2020-10-28 19:06:48 +0100

    Sync branches

--- src/main/scala/AverageJoes/common/ServerSearch.scala
+++ src/main/scala/AverageJoes/common/ServerSearch.scala
@@ -6,19 +6,19 @@ import akka.actor.typed.scaladsl.ActorContext
 
 trait ServerSearch {
   //import com.typesafe.config.Config
- /* //import com.typesafe.config.ConfigFactory
+  //import com.typesafe.config.ConfigFactory
   //val config = ConfigFactory.parseFile(new Nothing("src/main/scala/AverageJoes/client.conf"))
   //val system: Nothing = ActorSystem.create("MySystem", config)
   //
   //def context : ActorContext
   def server: ActorRef[GymController.Msg] =
-    {//ToDo: temporaneamente istanziato un gym controller
-      ServerSearch.serverDummy
-      //ActorSelection = context.actorSelection("akka://MySystem@127.0.0.1:25520/user/myActor")
-      //public scala.concurrent.Future<ActorRef> resolveOne(Timeout timeout)
-    }*/
+  {//ToDo: temporaneamente istanziato un gym controller
+    ServerSearch.serverDummy
+    //ActorSelection = context.actorSelection("akka://MySystem@127.0.0.1:25520/user/myActor")
+    //public scala.concurrent.Future<ActorRef> resolveOne(Timeout timeout)
+  }
 }
 
 object ServerSearch{
- // private val serverDummy: ActorSystem[GymController.Msg] = ActorSystem(GymController(), "GymController")
+  private val serverDummy: ActorSystem[GymController.Msg] = ActorSystem(GymController(), "GymController")
 }
