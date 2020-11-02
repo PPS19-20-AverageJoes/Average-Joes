af761585f785b7301f52b87bd8229a3d15be8877
Elena_96 <elena.morelli3@studio.unibo.it>
2020-11-02 11:29:22 +0100

    add gui module to set connection

--- src/main/scala/AverageJoes/view/ViewToolActor.scala
+++ src/main/scala/AverageJoes/view/ViewToolActor.scala
@@ -1,14 +1,15 @@
 package AverageJoes.view
 
-
-import akka.actor.typed.Behavior
+import AverageJoes.model.device.Device
+import AverageJoes.model.machine.PhysicalMachine
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import javax.swing.SwingUtilities
 
 import scala.swing.GridPanel
 
 
 sealed trait ViewToolActor extends AbstractBehavior[ViewToolActor.Msg] {
-  val panel: GridPanel
   val machineID: String
   var machine: GridPanel
 
@@ -28,20 +29,31 @@ object ViewToolActor {
   }
 
   case class ViewDeviceActor(override val context: ActorContext[Msg],
-                             override val panel: GridPanel, override val machineID: String)
+                             panel: UserView, override val machineID: String,
+                             actorRef: ActorRef[Device.Msg])
     extends AbstractBehavior[Msg](context) with ViewToolActor  {
-    override var machine: GridPanel = MachineGUI(machineID)
+
+    override var machine: GridPanel = UserGui(/*actorRef: ActorRef[Device.Msg]*/)
 
     override def createViewEntity(): Unit = {
-      panel.contents += machine
+      SwingUtilities.invokeLater(() => {
+        panel.contents += machine
+      })
     }
   }
 
   case class ViewPhysicalMachineActor(override val context: ActorContext[Msg],
-                                      override val panel: GridPanel, override val machineID: String)
+                                       panel: MachineView, override val machineID: String,
+                                      actorRef: ActorRef[PhysicalMachine.Msg])
     extends AbstractBehavior[Msg](context) with ViewToolActor {
-    override var machine: GridPanel = UserGui()
+
+    override var machine: GridPanel = MachineGUI()
+
     override def createViewEntity(): Unit = {
+      SwingUtilities.invokeLater(() => {
+        panel.contents += machine
+        panel.addEntry(machineID, actorRef)
+      })
 
     }
 
