a3fa39c679e4880e936f0e5eb53c2b014c0a5dc0
Andrea <andrea.campidelli@studio.unibo.it>
2020-11-08 18:03:10 +0100

    deviceID to customerID

--- src/main/scala/AverageJoes/model/device/Wristband.scala
+++ src/main/scala/AverageJoes/model/device/Wristband.scala
@@ -6,15 +6,15 @@ import akka.actor.typed.{ActorRef, Behavior}
 
 /**
  * AC
- * @param deviceID: ID of the device. In real devices, is stored on config files
+ * @param customerID: ID of the device. In real devices, is stored on config files
  */
-class Wristband(context: ActorContext[Device.Msg], override val deviceID: String) extends AbstractBehavior[Device.Msg](context) with Device {
+class Wristband(context: ActorContext[Device.Msg], override val customerID: String) extends AbstractBehavior[Device.Msg](context) with Device {
 
   def display (s: String): Unit = {
     println(s)
   }
 
-  def rfid(ref: ActorRef[PhysicalMachine.Msg]) : Unit = {ref ! PhysicalMachine.Msg.Rfid(deviceID)}
+  def rfid(ref: ActorRef[PhysicalMachine.Msg]) : Unit = {ref ! PhysicalMachine.Msg.Rfid(customerID)}
 
   override val logName: String = "Device Wristband"
   override val loggingContext: ActorContext[Device.Msg] = this.context
