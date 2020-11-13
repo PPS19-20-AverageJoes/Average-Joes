2f45095c4f96899ed2f285aa3695cca189cc413a
Sokol Guri <sokol.guri@studio.unibo.it>
2020-11-13 07:53:46 +0100

    CustomerActor handling exercise execution

--- src/main/scala/AverageJoes/model/customer/CustomerGroup.scala
+++ src/main/scala/AverageJoes/model/customer/CustomerGroup.scala
@@ -2,30 +2,31 @@ package AverageJoes.model.customer
 
 import AverageJoes.common.LoggableMsg
 import AverageJoes.controller.GymController.Msg.{CustomerList, CustomerRegistered}
-import AverageJoes.model.customer.CustomerGroup.CustomerLogin
+import AverageJoes.model.customer.CustomerActor.CustomerTrainingProgram
+import AverageJoes.model.customer.CustomerGroup.{CustomerLogin, UploadCustomerTaringProgram}
 import AverageJoes.model.device.Device
 import AverageJoes.model.device.Device.Msg.CustomerLogged
+import AverageJoes.model.fitness.TrainingProgram
 import AverageJoes.model.machine.MachineActor
 import AverageJoes.model.machine.MachineActor.Msg.CustomerLogging
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 
-/**
- * Group actor will handle the requests that will be passed by Customer Manager Actor
- */
 
 object CustomerGroup {
-  def apply(groupID: String): Behavior[Msg] = Behaviors.setup(ctx => new CustomerGroup(ctx, groupID))
+  def apply(groupID: String, manager: ActorRef[CustomerManager.Msg]): Behavior[Msg] = Behaviors.setup(ctx => new CustomerGroup(ctx, manager, groupID))
 
   trait Msg extends LoggableMsg
-  final case class CustomerLogin(customerId: String, machine: ActorRef[MachineActor.Msg], device: ActorRef[Device.Msg]) extends Msg
 
-  private final case class CustomerTerminated(device: ActorRef[CustomerActor.Msg], groupId: String, customerId: String) extends Msg
+  final case class CustomerLogin(customerId: String, machine: ActorRef[MachineActor.Msg], device: ActorRef[Device.Msg])   extends Msg
+  private final case class UploadCustomerTaringProgram(customerId: String, customer: ActorRef[CustomerActor.Msg])         extends Msg
+  private final case class CustomerTerminated(device: ActorRef[CustomerActor.Msg], groupId: String, customerId: String)   extends Msg
 }
 
-class CustomerGroup(ctx: ActorContext[CustomerGroup.Msg], groupId: String)
-  extends AbstractBehavior[CustomerGroup.Msg](ctx) {
 
+class CustomerGroup(ctx: ActorContext[CustomerGroup.Msg],
+                    manager: ActorRef[CustomerManager.Msg],
+                    groupId: String) extends AbstractBehavior[CustomerGroup.Msg](ctx) {
   import CustomerGroup.{CustomerTerminated, Msg}
   import CustomerManager._
 
@@ -35,12 +36,21 @@ class CustomerGroup(ctx: ActorContext[CustomerGroup.Msg], groupId: String)
 
     case RequestCustomerCreation(customerId, replyTo, _) =>
       customerIdToActor.get(customerId) match {
-        case Some(userActor) => replyTo ! CustomerRegistered(userActor)
+
+        case Some(customerActor) =>
+          replyTo ! CustomerRegistered(customerId, customerActor)
+          context.self ! UploadCustomerTaringProgram(customerId, customerActor)
         case None =>
-          val customerActor = context.spawn(CustomerActor(groupId, customerId), s"user-$customerId")
-          context.watchWith(customerActor, CustomerTerminated(customerActor, groupId, customerId))
-          customerIdToActor += customerId -> customerActor
-          replyTo ! CustomerRegistered(customerActor)
+          if(isCustomerOnStorage(customerId)) {
+            val customerActor = context.spawn(CustomerActor(manager, customerId), s"customer-$customerId")
+            context.watchWith(customerActor, CustomerTerminated(customerActor, groupId, customerId))
+            customerIdToActor += customerId -> customerActor
+            replyTo ! CustomerRegistered(customerId, customerActor)
+            context.self ! UploadCustomerTaringProgram(customerId, customerActor)
+          }
+          else{
+            /** Do something because customerId is not present on storage */
+          }
       }
       this
 
@@ -55,16 +65,27 @@ class CustomerGroup(ctx: ActorContext[CustomerGroup.Msg], groupId: String)
       }
       this
 
-    case RequestCustomerCreation(_, _, _) =>
-      this
-
     case RequestCustomerList(replyTo) =>
       replyTo ! CustomerList(customerIdToActor.values.toSet)
       this
 
+    case UploadCustomerTaringProgram(customerId, customer: ActorRef[CustomerActor.Msg]) =>
+      customer ! CustomerTrainingProgram(trainingProgramOf(customerId) )
+    this
 
-    case CustomerTerminated(_, _, userId) =>
-      customerIdToActor -= userId
+    case CustomerTerminated(_, _, customerId) =>
+      customerIdToActor -= customerId
       this
   }
+
+  private def isCustomerOnStorage(customerId: String): Boolean = {
+    /** TODO: Search for customer on database */
+    true
+  }
+
+  private def trainingProgramOf(customerId: String): TrainingProgram = {
+    /** TODO: Search for customer training program on database */
+    ???
+  }
+
 }
